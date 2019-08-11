f036cc47f0cf6212725202b06eed08e228aa7345
dennisgbrown <dennis.g.brown@gmail.com>
2019-08-10 22:38:02 -0500

    playing with audio recording

--- app/src/main/java/com/example/lsnr/MainActivity.java
+++ app/src/main/java/com/example/lsnr/MainActivity.java
@@ -1,5 +1,10 @@
 package com.example.lsnr;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.MediaRecorder;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -7,10 +12,14 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.core.app.ActivityCompat;
 
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,14 +30,73 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        System.out.println("OUTPUT");
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.RECORD_AUDIO },
+                    10);
+        }
+
+
+        int bufferSize = AudioRecord.getMinBufferSize(44100,
+                AudioFormat.CHANNEL_IN_MONO,
+                AudioFormat.ENCODING_PCM_16BIT);
+
+        System.out.println("BUFFER SIZE: " + bufferSize);
+
+        short[] audioBuffer = new short[bufferSize / 2];
+
+        AudioRecord record = new AudioRecord(MediaRecorder.AudioSource.DEFAULT,
+                44100,
+                AudioFormat.CHANNEL_IN_MONO,
+                AudioFormat.ENCODING_PCM_16BIT,
+                bufferSize);
+
+        //Log.v("HELLO", "HELLO2");
+
+        record.startRecording();
+
+        long shortsRead = 0;
+        boolean keepGoing = true;
+        while (keepGoing) {
+            int numberOfShort = record.read(audioBuffer, 0, audioBuffer.length);
+            shortsRead += numberOfShort;
+
+            //System.out.println(shortsRead);
+
+            System.out.print(averageIt(audioBuffer) + " ");
+            System.out.flush();
+            //printIt(audioBuffer);
+            //keepGoing = false;
+        }
+
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                System.out.println("CLICKED");
+                Snackbar.make(view, "Replace with your own action, yo", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    void printIt(short[] buffer)
+    {
+        for (short value : buffer) {
+            System.out.print(value + " ");
+        }
+        System.out.println();
+    }
+
+    int averageIt(short[] buffer)
+    {
+        int sum = 0;
+        for (short value : buffer) {
+            sum += value;
+        }
+        return sum / buffer.length;
     }
 
     @Override
